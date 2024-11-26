102a06d273624eb6b3e76e48c0c102c8502f95e2
Rijk Mulder <mulderrijk@gmail.com>
2024-11-26 14:14:48 +0100

    Simple sound done

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -2,48 +2,52 @@ using System;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class AudioManager : MonoBehaviour
+namespace Audio
 {
-    public static AudioManager Instance { get; private set; }
-    [SerializeField] private AudioMixer _mixer;
-    private SoundClip[] _audioClips;
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+        [SerializeField] private AudioMixer _mixer;
+        private SoundClip[] _audioClips;
 
-    private void Awake()
-    {
-        Instance = this;
+        private void Awake()
+        {
+            Instance = this;
         
-        // get all audio clips
-        _audioClips = Resources.LoadAll<SoundClip>("AudioClips");
-        foreach (var audioClip in _audioClips)
-        {
-            audioClip.Source = gameObject.AddComponent<AudioSource>();
-            audioClip.Source.clip = audioClip.Clip;
-            audioClip.Source.volume = audioClip.Volume;
-            audioClip.Source.pitch = audioClip.Pitch;
-            audioClip.Source.loop = audioClip.Loop;
-            audioClip.Source.outputAudioMixerGroup = _mixer.FindMatchingGroups(audioClip.SoundType.ToString())[0];
+            // get all audio clips
+            _audioClips = Resources.LoadAll<SoundClip>("AudioClips");
+            foreach (var audioClip in _audioClips)
+            {
+                audioClip.Source = gameObject.AddComponent<AudioSource>();
+                audioClip.Source.clip = audioClip.Clip;
+                audioClip.Source.volume = audioClip.Volume;
+                audioClip.Source.pitch = audioClip.Pitch;
+                audioClip.Source.loop = audioClip.Loop;
+                audioClip.Source.outputAudioMixerGroup = _mixer.FindMatchingGroups(audioClip.SoundType.ToString())[0];
+            }
         }
-    }
-    public void PlayClip(string clipName)
-    {
-        SoundClip clip = GetClip(clipName);
-        if (clip == null) return;
-        clip.Source.Play();
-    }
-    public void StopClip(string clipName)
-    {
-        SoundClip clip = GetClip(clipName);
-        if (clip == null) return;
-        clip.Source.Stop();
-    }
-    private SoundClip GetClip(string clipName)
-    {
-        SoundClip clip = Array.Find(_audioClips, sound => sound.Name == clipName);
-        if (clip == null)
+        public void PlayClip(string clipName)
         {
-            Debug.LogError($"Sound clip with name {clipName} not found!");
-            return null;
+            SoundClip clip = GetClip(clipName);
+            if (clip == null) return;
+            if (clip.Loop) clip.Source.loop = true;
+            clip.Source.Play();
         }
-        return clip;
+        public void StopClip(string clipName)
+        {
+            SoundClip clip = GetClip(clipName);
+            if (clip == null) return;
+            clip.Source.Stop();
+        }
+        private SoundClip GetClip(string clipName)
+        {
+            SoundClip clip = Array.Find(_audioClips, sound => sound.Name == clipName);
+            if (clip == null)
+            {
+                Debug.LogError($"Sound clip with name {clipName} not found!");
+                return null;
+            }
+            return clip;
+        }
     }
 }
--- Assets/Scripts/Audio/SoundClip.cs
+++ Assets/Scripts/Audio/SoundClip.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
-public enum SoundType
+namespace Audio
 {
-    Music,
-    SFX
-}
-[CreateAssetMenu(fileName = "New AudioClip", menuName = "Audio/AudioClip")]
-public class SoundClip : ScriptableObject
-{
-    public string Name;
-    public AudioClip Clip;
-    public SoundType SoundType;
-    public bool Loop;
-    public bool PlayOnAwake;
-    public float Volume;
-    public float Pitch;
-    public AudioSource Source;
+    public enum SoundType
+    {
+        Music,
+        Sfx
+    }
+    [CreateAssetMenu(fileName = "New AudioClip", menuName = "Audio/AudioClip")]
+    public class SoundClip : ScriptableObject
+    {
+        public string Name;
+        public AudioClip Clip;
+        public SoundType SoundType;
+        public bool Loop;
+        public bool PlayOnAwake;
+        public float Volume;
+        public float Pitch;
+        public AudioSource Source;
+    }
 }
