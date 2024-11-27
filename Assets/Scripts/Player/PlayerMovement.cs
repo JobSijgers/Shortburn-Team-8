d2f4dcda9a9e51640d46980f843d262d7371d180
Rijk Mulder <mulderrijk@gmail.com>
2024-11-27 22:46:03 +0100

    almost done

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,27 +1,74 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 namespace Player
 {
+    public enum EPlayerMovementState
+    {
+        Idle,
+        Walking,
+        Running,
+        Crouching,
+    }
+
     public class PlayerMovement : MonoBehaviour
     {
         [Header("Camera")] [SerializeField] private Transform _playerCamera;
         [SerializeField] private float _sensitivity = 10;
 
-        [Header("Movement")] [SerializeField] private CharacterController _controller;
-        [SerializeField] private float _speed = 5;
-        [SerializeField] private float _mass = 1;
+        [Header("Player settings: ", order = 0)] 
+        [SerializeField] public CharacterController Controller;
 
-        private Transform _constraintCenter;
-        private float _constraintRadius = 10;
+        [SerializeField] public float Gravity = -9.81f; 
+
+        [FormerlySerializedAs("walkSpeed")]
+        [Header("  1. Speed", order = 1)] 
+        [SerializeField] private float _walkSpeed;
+
+        [SerializeField] private float _speedChangeTime;
+        [SerializeField] private float _sprintMultiplier;
+
+        [FormerlySerializedAs("jumpHeight")]
+        [Header("  2. Jump", order = 2)] 
+        [SerializeField] private float _jumpHeight;
+
+        [FormerlySerializedAs("crouchHeight")]
+        [Header("  3. Crouch", order = 3)] 
+        [SerializeField] private float _crouchHeight;
+        [SerializeField] private float _crouchDuration;
+
 
         private PlayerInputActions _playerInputActions;
-        private InputAction _lookAction;
         private InputAction _moveAction;
+        private InputAction _jumpAction;
+        private InputAction _crouchAction;
+        private InputAction _sprintAction;
+        private InputAction _lookAction;
 
+        // public variables
+        [HideInInspector] public float speed;
+        [HideInInspector] public Vector3 Velocity;
+        [HideInInspector] public Vector3 MoveVector;
+        [HideInInspector] public EPlayerMovementState PlayerMovementState;
+
+        // private variables
+        private float _standHeight;
         private Vector2 _look;
-        private Vector3 _velocity;
+
+        // coroutines
+        private Coroutine _crouchCoroutine;
+        private Coroutine _speedChangeCoroutine;
+
+        // constraints
+        private Transform _constraintCenter;
+        private float _constraintRadius = 10f;
+
+        // events
+        public delegate void PlayerStateDelegate();
+
+        public PlayerStateDelegate OnPlayerStateChange;
 
         private void Awake()
         {
@@ -30,23 +77,102 @@ namespace Player
 
         private void Start()
         {
+            speed = _walkSpeed;
+            _standHeight = transform.localScale.y;
             Cursor.lockState = CursorLockMode.Locked;
         }
 
         private void OnEnable()
         {
-            _lookAction = _playerInputActions.Player.Look;
-            _lookAction.Enable();
+            // Initialize input actions
             _moveAction = _playerInputActions.Player.Move;
+            _jumpAction = _playerInputActions.Player.Jump;
+            _crouchAction = _playerInputActions.Player.Crouch;
+            _sprintAction = _playerInputActions.Player.Sprint;
+            _lookAction = _playerInputActions.Player.Look;
+            // Enable input actions
             _moveAction.Enable();
+            _jumpAction.Enable();
+            _crouchAction.Enable();
+            _sprintAction.Enable();
+            _lookAction.Enable();
         }
 
         private void Update()
         {
-            UpdateLook();
-            UpdateMovement();
-            UpdateGravity();
             ConstrainPosition();
+            UpdateLook();
+
+            // Handle grounded state
+            if (Controller.isGrounded && Velocity.y < 0)
+            {
+                Velocity.y = -2f;
+            }
+
+            // Movement
+            Vector2 input = _moveAction.ReadValue<Vector2>();
+            float x = input.x;
+            float z = input.y;
+
+            MoveVector = transform.right * x + transform.forward * z;
+            if (MoveVector.magnitude < 0.1f)
+            {
+                PlayerMovementState = EPlayerMovementState.Idle;
+            }
+            else
+            {
+                PlayerMovementState = PlayerMovementState == EPlayerMovementState.Running
+                    ? EPlayerMovementState.Running
+                    : EPlayerMovementState.Walking;
+            }
+
+            Controller.Move(MoveVector * speed * Time.deltaTime);
+
+            // Jump
+            if (_jumpAction.triggered && Grounded())
+            {
+                Velocity.y = Mathf.Sqrt(_jumpHeight * -2f * Gravity);
+            }
+
+            // Apply gravity
+            Velocity.y += Gravity * Time.deltaTime;
+            Controller.Move(Velocity * Time.deltaTime);
+
+            // Crouch
+            if (_crouchAction.IsPressed())
+            {
+                PlayerMovementState = EPlayerMovementState.Crouching;
+            }
+
+            if (_crouchAction.triggered)
+            {
+                if (_crouchCoroutine != null) StopCoroutine(_crouchCoroutine);
+                _crouchCoroutine = StartCoroutine(Crouch(true));
+                AdjustSpeed(_walkSpeed / 2);
+            }
+
+            if (_crouchAction.WasReleasedThisFrame())
+            {
+                if (_crouchCoroutine != null) StopCoroutine(_crouchCoroutine);
+                _crouchCoroutine = StartCoroutine(Crouch(false));
+                AdjustSpeed(_walkSpeed);
+                PlayerMovementState = EPlayerMovementState.Walking;
+            }
+
+            // Sprint
+            if (_sprintAction.IsPressed() && PlayerMovementState != EPlayerMovementState.Crouching)
+            {
+                PlayerMovementState = EPlayerMovementState.Running;
+                if (Grounded())
+                {
+                    AdjustSpeed(_walkSpeed * _sprintMultiplier);
+                }
+            }
+            else if (PlayerMovementState != EPlayerMovementState.Crouching)
+            {
+                PlayerMovementState = EPlayerMovementState.Walking;
+                AdjustSpeed(_walkSpeed);
+            }
         }
 
         private void UpdateLook()
@@ -58,22 +184,49 @@ namespace Player
             _playerCamera.localRotation = Quaternion.Euler(-_look.y, 0, 0);
         }
 
-        private void UpdateMovement()
+        public bool Grounded()
         {
-            Vector2 move = _moveAction.ReadValue<Vector2>();
-
-            Vector3 input = new();
-            input += transform.right * move.x;
-            input += transform.forward * move.y;
-            input = Vector3.ClampMagnitude(input, 1);
-
-            _controller.Move((input * _speed + _velocity) * Time.deltaTime);
+            float radius = transform.localScale.x / 2;
+            return Physics.SphereCast(transform.position, radius, Vector3.down, out RaycastHit hit,
+                transform.localScale.y);
         }
 
-        private void UpdateGravity()
+        private void AdjustSpeed(float newSpeed)
         {
-            Vector3 gravity = Physics.gravity * (Time.deltaTime * _mass);
-            _velocity.y = _controller.isGrounded ? -1f : _velocity.y + gravity.y;
+            if (_speedChangeCoroutine != null) StopCoroutine(_speedChangeCoroutine);
+            _speedChangeCoroutine = StartCoroutine(ChangeSpeed(newSpeed));
+            OnPlayerStateChange?.Invoke();
+        }
+
+        private IEnumerator Crouch(bool crouching)
+        {
+            float t = 0;
+            float desiredHeight = crouching ? _crouchHeight : _standHeight;
+            while (t < _crouchDuration)
+            {
+                t += Time.deltaTime;
+
+                float amount = t / _crouchDuration;
+                float y = Mathf.Lerp(transform.localScale.y, desiredHeight, amount);
+                transform.localScale = new Vector3(transform.localScale.x, y, transform.localScale.z);
+                transform.Translate(Vector3.up * 0.01f);
+                yield return null;
+            }
+
+            _crouchCoroutine = null;
+        }
+
+        private IEnumerator ChangeSpeed(float newSpeed)
+        {
+            float t = 0;
+            while (t < _speedChangeTime)
+            {
+                t += Time.deltaTime;
+                float amount = t / _speedChangeTime;
+                speed = Mathf.Lerp(speed, newSpeed, amount);
+                yield return null;
+            }
+            _speedChangeCoroutine = null;
         }
 
         private void ConstrainPosition()
@@ -81,24 +234,24 @@ namespace Player
             if (_constraintCenter == null) return;
 
             Vector3 offset = transform.position - _constraintCenter.position;
-            if (!(offset.magnitude > _constraintRadius)) 
+            if (!(offset.magnitude > _constraintRadius))
                 return;
+
             Vector3 constrainedPosition = _constraintCenter.position + offset.normalized * _constraintRadius;
-            _controller.enabled = false;
+            Controller.enabled = false;
             transform.position = constrainedPosition;
-            _controller.enabled = true;
+            Controller.enabled = true;
         }
-        
+
         public void SetConstraint(Transform center, float radius)
         {
             _constraintCenter = center;
             _constraintRadius = radius;
         }
-        
+
         public void RemoveConstraint()
         {
             _constraintCenter = null;
         }
-        
     }
 }
