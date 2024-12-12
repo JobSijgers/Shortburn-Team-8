49cedee2fbbcebfa35e1feb48a888dc94ef4c65d
Job SIjgers <jobsijgers@gmail.com>
2024-12-12 08:54:17 +0100

    fix speed = 0 issue in hand movemnet

--- Assets/Scripts/HandScripts/Core/Hand.cs
+++ Assets/Scripts/HandScripts/Core/Hand.cs
@@ -71,7 +71,12 @@ namespace HandScripts.Core
             transform.SetParent(null);
             Vector3 startPos = transform.position;
             Quaternion startRot = transform.rotation;
+            Debug.Log(startPos + " " + destination.position);
             float speed = Vector3.Distance(startPos, destination.position) / _metersPerSecond;
+            if (speed <= 0.2f)
+            {
+                speed = 0.2f;
+            }
 
 
             float t = 0f;
