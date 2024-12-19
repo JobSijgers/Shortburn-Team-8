9bd3c36ff45c9fd685e98b6bc36a61ec5acf486f
Rijk Mulder <mulderrijk@gmail.com>
2024-12-19 12:40:27 +0100

    Maze puzzle start

--- Assets/Scripts/Maze/Maze.cs
+++ Assets/Scripts/Maze/Maze.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using HandScripts.Core;
 using UnityEngine;
 using HandScripts.Pull;
@@ -6,21 +7,32 @@ namespace Maze
 {
     public class Maze : MonoBehaviour
     {
-        [SerializeField] private GameObject _mazeFollowObject;
+        [SerializeField] private MazePullable _mazeFollowObject;
         [SerializeField] private Transform _player;
         [SerializeReference] private MazeBlock _currentBlock;
         [SerializeReference] private MazeBlock _endBlock;
 
         private bool _isPastMidpoint = false;
-        
-        public void Pull(Vector3 currentLocation)
+        public Vector3 GetNextPosition()
         {
             bool currentBlockIsStraight = _currentBlock.IsStraight();
-            MazeBlock target = _currentBlock.GetNextMazeBlock(currentLocation);
+            MazeBlock target = _currentBlock.GetNextMazeBlock(_player.position);
+            if (_currentBlock == _endBlock) return _endBlock.transform.position;
             
+            Vector3 currentPos = _mazeFollowObject.transform.position;
+            Vector3 targetPos = target.transform.position;
+            Vector3 direction = targetPos - currentPos;
+            Vector3 nextPos = currentPos + direction.normalized * _mazeFollowObject.GetPullSpeed();
             
-        }
+            if (Vector3.Distance(nextPos, target.transform.position) < 0.05f)
+            {
+                _currentBlock = target;
+            }
+            // rotatefollow boject in direction of movement smoothly
+            _mazeFollowObject.transform.rotation = Quaternion.Slerp(_mazeFollowObject.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * 10f);
 
+            return nextPos;
+        }
         public void SetCurrentBlock(MazeBlock block)
         {
             _currentBlock = block;
--- Assets/Scripts/Maze/MazePullable.cs
+++ Assets/Scripts/Maze/MazePullable.cs
@@ -1,25 +1,28 @@
 using HandScripts.Core;
+using HandScripts.Grab;
 using HandScripts.Pull;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace Maze
 {
     public class MazePullable : MonoBehaviour, IHandInteractable, IHandPullable
     {
-        [SerializeField] private Transform _handHoldPoint;
-        [SerializeField] private float _pullSpeed;
+        [SerializeField] private GrabPoint _handHoldPoint;
+        [SerializeField] private float _pullStep;
         [SerializeField] private Maze _maze;
 
-        public Transform GetHeldPoint() => _handHoldPoint;
+        public GrabPoint GetGrabPoint() => _handHoldPoint;
         public EInteractType GetInteractType() => EInteractType.Pull;
         public Transform GetObjectTransform() => transform;
+        public float GetPullSpeed() => _pullStep;
         public bool CanPull(Vector3 playerPosition) => true;
         public bool HasBeenPulled() => false;
 
         public void Pull(UnityAction onComplete)
         {
-            throw new System.NotImplementedException();
+            transform.position = _maze.GetNextPosition();
         }
     }
 }
