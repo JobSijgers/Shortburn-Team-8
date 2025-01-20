2fb5ce0718820e4874a4749e2fa7be66b187113d
Job SIjgers <jobsijgers@gmail.com>
2025-01-20 08:55:28 +0100

    update to audiomanager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -6,48 +7,88 @@ namespace Audio
 {
     public class AudioManager : MonoBehaviour
     {
-        public static AudioManager Instance { get; private set; }
-        [SerializeField] private AudioMixer _mixer;
-        private SoundClip[] _audioClips;
+        [Serializable]
+        public class Sound
+        {
+            public string name;
+            public AudioClip clip;
+            [Range(0f, 1f)] public float volume = 0.7f;
+            [Range(0.5f, 1.5f)] public float pitch = 1f;
+            public bool loop;
+            public bool playOnAwake;
+            public bool disableCutoff;
+            [Range(0f, 1f)] public float cutoffRange;
+            public AudioMixerGroup mixerGroup;
+            [HideInInspector] public AudioSource source;
+        }
 
-        private void Awake()
+        public static AudioManager instance;
+        [SerializeField] private Sound[] sounds;
+
+        private void Awake() => instance = this;
+        private readonly Dictionary<string, Sound> soundDictionary = new();
+
+        private void Start()
         {
-            Instance = this;
-        
-            // get all audio clips
-            _audioClips = Resources.LoadAll<SoundClip>("AudioClips");
-            foreach (var audioClip in _audioClips)
+            //play sounds that are set to play on awake
+            foreach (Sound sound in sounds)
             {
-                audioClip.Source = gameObject.AddComponent<AudioSource>();
-                audioClip.Source.clip = audioClip.Clip;
-                audioClip.Source.volume = audioClip.Volume;
-                audioClip.Source.pitch = audioClip.Pitch;
-                audioClip.Source.loop = audioClip.Loop;
-                audioClip.Source.outputAudioMixerGroup = _mixer.FindMatchingGroups(audioClip.SoundType.ToString())[0];
+                if (sound.clip == null)
+                {
+                    Debug.LogWarning($"Sound clip: {sound.name} is null");
+                    continue;
+                }
+
+                soundDictionary.Add(sound.name, sound);
+                CreateAudioSource(sound);
+            }
+
+            sounds = null;
+        }
+
+        //find sound and play it
+        public void PlaySound(string soundName)
+        {
+            if (!soundDictionary.TryGetValue(soundName, out Sound sound))
+            {
+                return;
+            }
+
+            if (!sound.source.isPlaying)
+            {
+                sound.source.Play();
+                return;
+            }
+
+            if (sound.disableCutoff && sound.source.time > sound.clip.length * sound.cutoffRange)
+            {
+                sound.source.Play();
             }
         }
-        public void PlayClip(string clipName)
+
+        public void StopSound(string soundName)
         {
-            SoundClip clip = GetClip(clipName);
-            if (clip == null) return;
-            if (clip.Loop) clip.Source.loop = true;
-            clip.Source.Play();
-        }
-        public void StopClip(string clipName)
-        {
-            SoundClip clip = GetClip(clipName);
-            if (clip == null) return;
-            clip.Source.Stop();
-        }
-        private SoundClip GetClip(string clipName)
-        {
-            SoundClip clip = Array.Find(_audioClips, sound => sound.Name == clipName);
-            if (clip == null)
+            if (!soundDictionary.TryGetValue(soundName, out Sound sound))
             {
-                Debug.LogError($"Sound clip with name {clipName} not found!");
-                return null;
+                return;
             }
-            return clip;
+
+            sound.source.Stop();
+        }
+
+        private void CreateAudioSource(Sound sound)
+        {
+            AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.clip = sound.clip;
+            audioSource.volume = sound.volume;
+            audioSource.pitch = sound.pitch;
+            audioSource.loop = sound.loop;
+            audioSource.outputAudioMixerGroup = sound.mixerGroup;
+            sound.source = audioSource;
+            if (sound.playOnAwake)
+            {
+                audioSource.Play();
+            }
         }
     }
 }
