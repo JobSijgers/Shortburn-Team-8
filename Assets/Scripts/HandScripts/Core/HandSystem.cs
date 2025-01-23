d9c3ec471954e554db9b018a69da1f9e2785717c
Job Sijgers <jobsijgers@gmail.com>
2025-01-24 00:04:35 +0100

    Removed error when ray hits nothing

--- Assets/Scripts/HandScripts/Core/HandSystem.cs
+++ Assets/Scripts/HandScripts/Core/HandSystem.cs
@@ -12,6 +12,7 @@ namespace HandScripts.Core
     public class HandSystem : MonoBehaviour
     {
         public static HandSystem Instance;
+
         [Header("Hand Settings")]
         [SerializeField] private Hand _rightHand;
 
@@ -77,8 +78,11 @@ namespace HandScripts.Core
             IHandInteractable[] interactables = hit.collider?.GetComponents<IHandInteractable>();
             float closestDistance = float.MaxValue;
             IHandInteractable interactable = null;
+            if (interactables == null)
+                return;
             foreach (IHandInteractable current in interactables)
             {
+                
                 GrabPoint grabPoint = current.GetGrabPoint();
                 float distance = Vector3.Distance(transform.position, grabPoint.GrabPointTransform.position);
                 if (distance < closestDistance)
@@ -113,7 +117,7 @@ namespace HandScripts.Core
 
         private RaycastHit ShootRay()
         {
-                Physics.Raycast(_rayOrigin.position, _rayOrigin.forward, out RaycastHit hit, _rayDistance, _layerMask);
+            Physics.Raycast(_rayOrigin.position, _rayOrigin.forward, out RaycastHit hit, _rayDistance, _layerMask);
             return hit;
         }
 
@@ -137,7 +141,7 @@ namespace HandScripts.Core
                 storedObject.SetParent(_rightHand.GetStoragePoint().transform);
                 storedObject.Released();
                 storedObject.ResetPosition(quaternion.identity);
-                
+
                 // set finger positions
                 IHandInteractable handInteractable = (IHandInteractable)storedObject;
                 _rightHand.MoveFingersToGrabPoint(handInteractable.GetGrabPoint(), true);
